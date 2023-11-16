b69018688340c82e866e610daa883569a51064ba
a-bashiri <abdulazizbashiri@gmail.com>
2023-11-16 07:23:42 +0300

    adding security configs

---
+++ src/main/java/com/example/quiz/Config/SecurityConfig.java
@@ -0,0 +1,76 @@
+package com.example.quiz.Config;
+
+
+import com.example.quiz.Service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final UserService userService;
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider=new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService);
+        authenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        String[] adminOnly = {"/api/restaurant/create","/api/restaurant/update","/api/user/delete"};
+        String[] userOnly = {"/api/order/update","/api/order/by-restaurant-id/**"};
+        String[] customerOnly = {"/api/order/create","/api/order/customer"};
+
+
+        http.cors().and()
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .and()
+                .authenticationProvider(authenticationProvider())
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST,"/api/user/register").permitAll()
+                .requestMatchers(HttpMethod.POST,"/api/user/login").permitAll()
+                .requestMatchers(adminOnly).hasAuthority("ADMIN")
+                .requestMatchers(userOnly).hasAuthority("USER")
+                .requestMatchers(customerOnly).hasAuthority("CUSTOMER")
+                .anyRequest().authenticated()
+                .and()
+                .logout().logoutUrl("/api/logout")
+                .deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true)
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+}
